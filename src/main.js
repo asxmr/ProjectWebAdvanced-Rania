5a4392c7314cd5e712705bc71643fc5f8b5f09b0
asxmr <rania.azaoum@student.ehb.be>
2025-07-03 18:33:51 +0200

    Rijen opvullen met nodige informatie

--- src/main.js
+++ src/main.js
@@ -15,3 +15,30 @@ const puntenSpan = document.getElementById("punten");
 const puntenDisplay = document.getElementById("punten-display");
 const searchInput = document.getElementById("search");
 const sortSelect = document.getElementById("sort");
+
+function createPersonnageRow(personnage) {
+  const row = document.createElement("tr");
+
+  row.innerHTML = `
+    <td><img src="${personnage.image}" alt="${personnage.name}" class="table-img" /></td>
+    <td>${personnage.name}</td>
+    <td>${personnage.status}</td>
+    <td>${personnage.species}</td>
+    <td>${personnage.gender}</td>
+    <td>${personnage.origin.name}</td>
+    <td>${personnage.importance}</td>
+    <td><button class="collect-btn">Toevoegen</button></td>
+  `;
+
+
+  row.querySelector(".collect-btn").addEventListener("click", () => {
+    if (!collection.find((c) => c.id === personnage.id)) {
+      collection.push(personnage);
+      saveCollection();
+      updateCollection();
+      alert(`${personnage.name} is toegevoegd aan je collectie!`);
+    }
+  });
+
+  return row;
+}
